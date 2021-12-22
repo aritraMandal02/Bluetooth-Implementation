24024815cf2d344d23b7594d85e09e25270dd606
Aritra Mandal <aritra.mandal2002@gmail.com>
2021-12-22 17:58:58 +0530

    Nearby devices showing

--- app/src/main/java/com/example/arduinobluetoothconnent/MainActivity.java
+++ app/src/main/java/com/example/arduinobluetoothconnent/MainActivity.java
@@ -4,15 +4,21 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.Toast;
+
+import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btOn, btOff;
+    Button btOn, btOff, btn;
+    ListView listView;
     BluetoothAdapter bluetoothAdapter;
     Intent btEnableIntent;
     int requestCodeForEnable;
@@ -23,12 +29,35 @@ public class MainActivity extends AppCompatActivity {
 
         btOn = findViewById(R.id.btOn);
         btOff = findViewById(R.id.btOff);
+        btn = findViewById(R.id.button3);
+        listView = findViewById(R.id.listview);
         bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
         btEnableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
         requestCodeForEnable = 1;
 
         bluetoothOnMethod();
         bluetoothOffMethod();
+        exeButton();
+    }
+
+    private void exeButton() {
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Set<BluetoothDevice> bt = bluetoothAdapter.getBondedDevices();
+                String[] strings = new String[bt.size()];
+                int index = 0;
+
+                if(bt.size() > 0){
+                    for(BluetoothDevice device:bt){
+                        strings[index] = device.getName();
+                        index++;
+                    }
+                    ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_list_item_1, strings);
+                    listView.setAdapter(arrayAdapter);
+                }
+            }
+        });
     }
 
     @Override
